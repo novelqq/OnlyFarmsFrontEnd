edac89c700bea1b5c36de07d2fab00c88e1d3b96
Ryan Zacharias <46061987+novelqq@users.noreply.github.com>
2020-11-30 23:02:44 -0800

    Update url.js

--- src/constants/url.js
+++ src/constants/url.js
@@ -1,2 +1,2 @@
-export const INVENTORY_MS = "https://7ybj2whv04.execute-api.us-east-1.amazonaws.com/dev"
-export const CART_MS = "https://gr01no9opa.execute-api.us-east-1.amazonaws.com/dev"
+export const INVENTORY_MS = "https://7ql1tbjad9.execute-api.us-east-1.amazonaws.com/dev"
+export const CART_MS = "https://4cxn6lgs0m.execute-api.us-east-1.amazonaws.com/dev"
